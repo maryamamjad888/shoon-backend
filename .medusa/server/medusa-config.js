cba8cea0fd5e6ae60b1326efd6c19f3efea5c99b
Maryam <apple@MacBook-Pro.local>
2025-05-30 14:55:46 +0500

    test

--- .medusa/server/medusa-config.js
+++ .medusa/server/medusa-config.js
@@ -1,19 +1,38 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const utils_1 = require("@medusajs/framework/utils");
-(0, utils_1.loadEnv)(process.env.NODE_ENV || 'development', process.cwd());
-module.exports = (0, utils_1.defineConfig)({
-    projectConfig: {
-        databaseUrl: process.env.DATABASE_URL,
-        http: {
-            storeCors: process.env.STORE_CORS,
-            adminCors: process.env.ADMIN_CORS,
-            authCors: process.env.AUTH_CORS,
-            jwtSecret: process.env.JWT_SECRET || "supersecret",
-            cookieSecret: process.env.COOKIE_SECRET || "supersecret"
-        }
-    },
 
+// Load environment variables
+(0, utils_1.loadEnv)(process.env.NODE_ENV || 'development', process.cwd());
+
+module.exports = (0, utils_1.defineConfig)({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+      storeCors: process.env.STORE_CORS,
+      adminCors: process.env.ADMIN_CORS,
+      authCors: process.env.AUTH_CORS,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret"
+    }
+  },
+
+  // ✅ Add this section for modules that require database config
+  modules: {
+    [utils_1.Modules.STOCK_LOCATION]: {
+      resolve: "@medusajs/stock-location",
+      options: {
+        clientUrl: process.env.DATABASE_URL, // use the same DB unless you’ve separated it
+      },
+    },
+    [utils_1.Modules.FULFILLMENT]: {
+      resolve: "@medusajs/fulfillment",
+      options: {
+        clientUrl: process.env.DATABASE_URL,
+      },
+    },
+  },
 });
+
 
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWVkdXNhLWNvbmZpZy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL21lZHVzYS1jb25maWcudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxxREFBaUU7QUFFakUsSUFBQSxlQUFPLEVBQUMsT0FBTyxDQUFDLEdBQUcsQ0FBQyxRQUFRLElBQUksYUFBYSxFQUFFLE9BQU8sQ0FBQyxHQUFHLEVBQUUsQ0FBQyxDQUFBO0FBRTdELE1BQU0sQ0FBQyxPQUFPLEdBQUcsSUFBQSxvQkFBWSxFQUFDO0lBQzVCLGFBQWEsRUFBRTtRQUNiLFdBQVcsRUFBRSxPQUFPLENBQUMsR0FBRyxDQUFDLFlBQVk7UUFDckMsSUFBSSxFQUFFO1lBQ0osU0FBUyxFQUFFLE9BQU8sQ0FBQyxHQUFHLENBQUMsVUFBVztZQUNsQyxTQUFTLEVBQUUsT0FBTyxDQUFDLEdBQUcsQ0FBQyxVQUFXO1lBQ2xDLFFBQVEsRUFBRSxPQUFPLENBQUMsR0FBRyxDQUFDLFNBQVU7WUFDaEMsU0FBUyxFQUFFLE9BQU8sQ0FBQyxHQUFHLENBQUMsVUFBVSxJQUFJLGFBQWE7WUFDbEQsWUFBWSxFQUFFLE9BQU8sQ0FBQyxHQUFHLENBQUMsYUFBYSxJQUFJLGFBQWE7U0FDekQ7S0FDRjtDQUNGLENBQUMsQ0FBQSJ9
