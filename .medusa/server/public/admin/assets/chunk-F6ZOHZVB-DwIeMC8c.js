8c2bc59c4f254d5300d70e4e8538c9d0e775a963
Maryam <apple@MacBook-Pro.local>
2025-05-31 11:54:30 +0500

    test

--- .medusa/server/public/admin/assets/chunk-F6ZOHZVB-DwIeMC8c.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CdNgUVYG.js";var e=({children:s})=>r.jsx("span",{className:"sr-only",children:s});export{e as V};
--- .medusa/server/public/admin/assets/chunk-XBDYD7NP-BDdteYTM.js
+++
@@ -1,1 +0,0 @@
-import{al as i}from"./index-CdNgUVYG.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
